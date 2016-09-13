089dcc52d7cdfa37e76c143c9076918695353cbe
wccosby <cosby.william.c@gmail.com>
2016-09-13 13:13:21 -0700

    simple def for early db structure, can make this more complicated later on

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,73 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-import os
-basedir = os.path.abspath(os.path.dirname(__file__))
-
+from models import User, Story, Question, n2nModel
 
 app = Flask(__name__)
-# app.config.from_object('config')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'app.db')
+app.config.from_object('config')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data/app.db'
 db = SQLAlchemy(app)
-from app import views, models
+
+##TODO find a better way to separate this code
+class User(db.Model):
+    """An admin user capable of viewing reports.
+    :param str email: email address of user
+    :param str password: encrypted password for the user
+    """
+    __tablename__ = 'user'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    email = db.Column(db.String, unique=True)
+    password = db.Column(db.String)
+    authenticated = db.Column(db.Boolean, default=False)
+
+    def is_active(self):
+        """True, as all users are active."""
+        return True
+
+    def get_id(self):
+        """Return the email address to satify Flask-Login's requirements."""
+        return self.email
+
+    def is_authenticated(self):
+        """Return True if the user is authenticated."""
+        return self.authenticated
+
+    def is_anonymous(self):
+        """False, as anonymous users aren't supported."""
+        return False
+
+    def __repr__(self):
+        return '<User %r>' % (self.name)
+
+class Story(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    story = db.Column(db.String)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    '''
+    handles how the db entry is printed if it needs to be
+    '''
+    def __repr__(self):
+        return '<Post %r>' % (self.body)
+
+class Question(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    question = db.Column(db.String)
+    answer = db.Column(db.String)
+    story_id = db.Column(db.Integer, db.ForeignKey('story.id'))
+
+    def __repr__(self):
+        return '<Post %r + %r>' % (self.question, self.answer)
+
+class n2nModel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    story_id = db.Column(db.Integer, db.ForeignKey('story.id'))
+
+    def __repr__(self):
+        return '<Post %r>' % (self.id)
+
+
+
+from app import views
--- app/models.py
+++ app/models.py
@@ -1,68 +1,66 @@
-"""Database models for the Bull application."""
-
-import datetime
-
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
-
-class User(db.Model):
-    """An admin user capable of viewing reports.
-    :param str email: email address of user
-    :param str password: encrypted password for the user
-    """
-    __tablename__ = 'user'
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    email = db.Column(db.String, unique=True)
-    password = db.Column(db.String)
-    authenticated = db.Column(db.Boolean, default=False)
-
-    def is_active(self):
-        """True, as all users are active."""
-        return True
-
-    def get_id(self):
-        """Return the email address to satify Flask-Login's requirements."""
-        return self.email
-
-    def is_authenticated(self):
-        """Return True if the user is authenticated."""
-        return self.authenticated
-
-    def is_anonymous(self):
-        """False, as anonymous users aren't supported."""
-        return False
-
-    def __repr__(self):
-        return '<User %r>' % (self.name)
-
-
-class Story(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    story = db.Column(db.String)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-
-    '''
-    handles how the db entry is printed if it needs to be
-    '''
-    def __repr__(self):
-        return '<Post %r>' % (self.body)
-
-class Question(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    question = db.Column(db.String)
-    answer = db.Column(db.String)
-    story_id = db.Column(db.Integer, db.ForeignKey('story.id'))
-
-    def __repr__(self):
-        return '<Post %r + %r>' % (self.question, self.answer)
-
-class n2nModel(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    story_id = db.Column(db.Integer, db.ForeignKey('story.id'))
-
-    def __repr__(self):
-        return '<Post %r>' % (self.id)
+# """Database models for the Bull application."""
+#
+# from flask_sqlalchemy import SQLAlchemy
+#
+# db = SQLAlchemy()
+#
+# class User(db.Model):
+#     """An admin user capable of viewing reports.
+#     :param str email: email address of user
+#     :param str password: encrypted password for the user
+#     """
+#     __tablename__ = 'user'
+#
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String)
+#     email = db.Column(db.String, unique=True)
+#     password = db.Column(db.String)
+#     authenticated = db.Column(db.Boolean, default=False)
+#
+#     def is_active(self):
+#         """True, as all users are active."""
+#         return True
+#
+#     def get_id(self):
+#         """Return the email address to satify Flask-Login's requirements."""
+#         return self.email
+#
+#     def is_authenticated(self):
+#         """Return True if the user is authenticated."""
+#         return self.authenticated
+#
+#     def is_anonymous(self):
+#         """False, as anonymous users aren't supported."""
+#         return False
+#
+#     def __repr__(self):
+#         return '<User %r>' % (self.name)
+#
+#
+# class Story(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     story = db.Column(db.String)
+#     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+#
+#     '''
+#     handles how the db entry is printed if it needs to be
+#     '''
+#     def __repr__(self):
+#         return '<Post %r>' % (self.body)
+#
+# class Question(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     question = db.Column(db.String)
+#     answer = db.Column(db.String)
+#     story_id = db.Column(db.Integer, db.ForeignKey('story.id'))
+#
+#     def __repr__(self):
+#         return '<Post %r + %r>' % (self.question, self.answer)
+#
+# class n2nModel(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+#     story_id = db.Column(db.Integer, db.ForeignKey('story.id'))
+#
+#     def __repr__(self):
+#         return '<Post %r>' % (self.id)
