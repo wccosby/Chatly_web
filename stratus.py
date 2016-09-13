7e54dc8c6404b48f678ce3c057f6532c287e8107
wccosby <cosby.william.c@gmail.com>
2016-09-12 18:44:30 -0700

    getting flask structure in place

---
+++ stratus.py
@@ -0,0 +1,54 @@
+from flask import Flask, jsonify, request, render_template
+
+import tensorflow as tf
+import numpy as np
+import pandas as pd
+
+#---------- MODEL IN MEMORY ----------------#
+
+# Read the scientific data on breast cancer survival,
+# Build a LogisticRegression predictor on it
+# patients = pd.read_csv("haberman.data", header=None)
+# patients.columns=['age','year','nodes','survived']
+# patients=patients.replace(2,0)  # The value 2 means death in 5 years
+#
+# X = patients[['age','year','nodes']]
+# Y = patients['survived']
+# PREDICTOR = LogisticRegression().fit(X,Y)
+
+
+#---------- URLS AND WEB PAGES -------------#
+
+# Initialize the app
+app = Flask(__name__)
+
+# Homepage
+@app.route("/")
+def index():
+    """
+    Homepage: serve our visualization page, index.html
+    """
+    return render_template("index.html")
+
+# Get an example and return it's score from the predictor model
+@app.route("/score/", methods=["POST"])
+def score():
+    """
+    When A POST request with json data is made to this uri,
+    Read the example from the json, predict probability and
+    send it with a response
+    """
+    # # Get decision score for our example that came with the request
+    # data = request.json
+    # x = np.matrix(data["example"])
+    # score = PREDICTOR.predict_proba(x)
+    # # Put the result in a nice dict so we can send it as json
+    # results = {"score": score[0,1]}
+    # return jsonify(results)
+    raise Exception("Implement this!")
+
+#--------- RUN WEB APP SERVER ------------#
+
+# Start the app server
+if __name__ == '__main__':
+    app.run(debug=True)
